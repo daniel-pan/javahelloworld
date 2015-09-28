3e217a29dc2492b2628686ecca2dd16fbb042ea1
daniel-pan <pandazhao2012@gmail.com>
2015-09-28 15:51:28 -0400

    write to /output/output.txt

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,7 +1,23 @@
+import java.io.Writer;
+import java.io.FileWriter;
 public class JavaHelloWorld{
-	public static void main(String[] args){
-		System.out.println("Java Hello World");
-		System.out.println("Hello again");
-		System.out.println("Hello again");
-	}
+        public static void main(String[] args){
+                System.out.println("Java Hello World");
+                System.out.println("Hello again");
+                Writer w = null;
+                try {                   
+                        String s = "/output/output.txt";
+                        System.out.println(s);
+                        w = new FileWriter(s);
+                        w.write("hello");
+                }catch (Exception e){
+                }finally {
+                        if (w != null){
+                                try {
+                                        w.close();
+                                }catch(Exception e){
+                                }
+                        }
+                }
+        }
 }
